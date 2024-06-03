2e8c24162926cbafb8b632aec9cb1c8a83226cb8
ra9miry <telmanradmirgo@gmail.com>
2024-06-03 08:23:51 +0500

    fix

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
+import { MessagesService } from './messages.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  providers: [MessagesService, PrismaService],
   controllers: [MessagesController],
 })
 export class MessagesModule {}
